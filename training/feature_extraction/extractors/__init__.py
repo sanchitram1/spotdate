505e62066883f13bd71d35b9897ab083aa6cc7f8
Sanchit Ram Arvind <sanchitram@gmail.com>
2026-03-15 00:54:09 -0700

    get basic out

--- training/feature_extraction/extractors/__init__.py
+++ training/feature_extraction/extractors/__init__.py
@@ -5,7 +5,8 @@ from training.feature_extraction._contract import FeatureExtractor
 # from .album import extract as album_extract
 from .artist import extract as artist_extract
 from .audio_features import extract as audio_features_extract
-from .basic import extract as basic_extract
+
+# from .basic import extract as basic_extract
 from .genre import extract as genre_extract
 from .temporal import extract as temporal_extract
 
@@ -13,7 +14,7 @@ ExtractorEntry = Tuple[str, FeatureExtractor]
 
 
 EXTRACTORS: List[ExtractorEntry] = [
-    ("basic", basic_extract),
+    # ("basic", basic_extract),
     ("temporal", temporal_extract),
     ("genre", genre_extract),
     ("audio_features", audio_features_extract),
