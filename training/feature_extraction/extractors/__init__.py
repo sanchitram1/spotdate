6d142125d9e81ec41ec3b1e496ffa0122a873735
Sanchit Ram Arvind <sanchitram@gmail.com>
2026-03-15 00:53:04 -0700

    artist

--- training/feature_extraction/extractors/__init__.py
+++ training/feature_extraction/extractors/__init__.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 from training.feature_extraction._contract import FeatureExtractor
 
 # from .album import extract as album_extract
-# from .artist import extract as artist_extract
+from .artist import extract as artist_extract
 from .audio_features import extract as audio_features_extract
 from .basic import extract as basic_extract
 from .genre import extract as genre_extract
@@ -18,5 +18,5 @@ EXTRACTORS: List[ExtractorEntry] = [
     ("genre", genre_extract),
     ("audio_features", audio_features_extract),
     # ("album", album_extract),
-    # ("artist", artist_extract),
+    ("artist", artist_extract),
 ]
